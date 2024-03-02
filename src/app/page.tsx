41dcb31ccd58e71d10ffc87c028e789264a80b5c
jerryCelozzi <jerrycelozzi@gmail.com>
2024-03-02 11:04:00 +0100

    package.json copiato da https://github.com/aindrila22/next-slider/blob/main/package.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,54 @@
-"use client"
-import { Box, Grid ,Stack} from '@mui/material'
-import React from 'react'
-import Image from 'next/image';
-
+"use client";
+import { Box, Grid, Stack } from "@mui/material";
+import React from "react";
+import Image from "next/image";
 
 function page() {
   return (
-   <Grid container sx={{backgroundColor:'white',minHeight:'75vh'}}>
-    <Grid xs={12}item  style={{display:'flex',justifyContent:'center',marginTop:'4%'}}>
-    <Grid container>
-      <Grid item md={2} ></Grid>
-      <Grid item xs={12} md={8}  sx={{display:'flex',justifyContent:'center'}}>
-        
-      <Box
+    <Grid container sx={{ backgroundColor: "white", minHeight: "75vh" }}>
+      <Grid xs={12} item style={{ display: "flex", justifyContent: "center" }}>
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            sx={{ display: "flex", justifyContent: "center",alignItems:'center' }}
+       
+          >
+            <Stack border={2} >
+              <Box
                 component="img"
                 sx={{
-                  height:350,
-                  display: 'block',
-                  
-                  overflow: 'hidden',
-                  width: '100%',
-             
+                  height: 350,
+                  display: "block",
+
+                  overflow: "hidden",
+                  width: "100%",
                 }}
-                src={'/logo.png'}
-               
+                src={"/logo.png"}
               />
-        </Grid>
-      <Grid item  md={2} ></Grid>
-      
-      </Grid>
+            </Stack>
+          </Grid>
+          {/* 
+
+        *** Image e prop Layout ***
         
-      </Grid>  
-   </Grid>
-  )
+        <Grid item xs={12} style={{display:'flex',justifyContent:'center'}}>
+            <Stack width={'50%'}>
+                <Image
+                    src="/logo.png"
+                    alt="cityopensource"
+                    width={400}
+                    height={10}
+                    layout="responsive"
+                />
+            </Stack>
+        </Grid> 
+    
+        */}
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
 
-export default page
+export default page;
